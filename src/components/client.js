d9e303a424a74048acd6566460a2db72ae0425bd
Denis <rojkoff.d@gmail.com>
2021-09-23 22:49:12 +0300

    props

--- src/components/client.js
+++ src/components/client.js
@@ -1,31 +1,30 @@
 import React from "react"
 
-const Client = ()=>{
+const Client = (clientProps, newClientsProps)=>{
 
     return (
         <div>
             <ul className="list-group">
-          {clients.map(
-            (client)=>
+          {newClientsProps.map(
+            (clientProps)=>
               {
                 return (
-                          <div className='d-flex mt-3 shadow  test' key={client.id}>
+                          <div className='d-flex mt-3 shadow  test' key={clientProps.id}>
                             <div className="row">
                               <div className="col-sm">
                                 <img src={logoA} width="100" height="100"/>
                               </div>
                               <div className="col-sm test">
-                                Телефон:{client.tel},
-                                {client.date},
-                                ФИО:{client.clientName.firstName},
-                                {client.clientName.lastName},
-                                {client.clientName.patronymic},
-                                Адрес:{client.address.city},
-                                {client.address.street},
-                                {client.address.home},
-                                {client.address.housing},
-                                {client.address.apartment},
-                                Статус:{client.statusClient}
+                                Телефон:{clientProps.tel},
+                                ФИО:{clientProps.clientName.firstName},
+                                {clientProps.clientName.lastName},
+                                {clientProps.clientName.patronymic},
+                                Адрес:{clientProps.address.city},
+                                {clientProps.address.street},
+                                {clientProps.address.home},
+                                {clientProps.address.housing},
+                                {clientProps.address.apartment},
+                                Статус:{clientProps.statusClient}
                               </div>
                               <div>
                                 {d.toLocaleString()}
